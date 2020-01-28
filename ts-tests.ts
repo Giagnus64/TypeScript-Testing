f2985429172f27e2a097aacad9b1189592bf1f12
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2020-01-28 16:57:04 -0500

    First commit, setting up test environment

---
+++ ts-tests.ts
@@ -0,0 +1,12 @@
+const numButton = document.querySelector(".add-numbers");
+const strButton = document.querySelector(".add-strings");
+const bothButton = document.querySelector(".add-both");
+const numInput = document.querySelector(".num-input");
+const strInput = document.querySelector(".string-input");
+const bothInput = document.querySelector(".both-input");
+
+function add(thing1, thing2){
+    thing1 + thing2
+}
+
+b
