9adbb522240a6c72d2dae996dd3ce8bd2afac2b5
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2020-01-29 18:01:19 -0500

    Finalized second typescript tests

--- ts-tests.js
+++ ts-tests.js
@@ -1,39 +1,38 @@
-//WITHOUT TYPING
-var numButton = document.querySelector(".add-numbers");
-var strButton = document.querySelector(".add-with-string");
-var num1Input = document.querySelector(".num1-input");
-var num2Input = document.querySelector(".num2-input");
-var strInput = document.querySelector(".string-input");
-function add(thing1, thing2, phrase, showString) {
-    return showString ? phrase + (thing1 + thing2) : thing1 + thing2;
-}
-numButton.addEventListener("click", function (e) {
-    console.log(add(parseInt(num1Input.value), parseInt(num2Input.value), strInput.value, false));
-});
-strButton.addEventListener("click", function (e) {
-    console.log(add(parseInt(num1Input.value), parseInt(num2Input.value), strInput.value, true));
-});
-// // WITH TYPING
-// const numButton = document.querySelector(".add-numbers")! as HTMLButtonElement;
-// const strButton = document.querySelector(".add-with-string")! as HTMLButtonElement;
-// const num1Input = document.querySelector(".num1-input")! as HTMLInputElement;
-// const num2Input = document.querySelector(".num2-input")! as HTMLInputElement;
-// const strInput = document.querySelector(".string-input")! as HTMLInputElement;
-// function add(
-//   thing1: number,
-//   thing2: number,
-//   phrase: string,
-//   showString: boolean
-// ) {
-//   return showString ? thing1 + thing2 + phrase : thing1 + thing2;
+// //WITHOUT TYPING
+// const numButton = document.querySelector(".add-numbers");
+// const strButton = document.querySelector(".add-with-string");
+// const num1Input = document.querySelector(".num1-input");
+// const num2Input = document.querySelector(".num2-input");
+// const strInput = document.querySelector(".string-input");
+// function add(thing1, thing2, phrase, showString){
+//    return showString ? phrase + (thing1 + thing2) : thing1 + thing2
 // }
-// numButton.addEventListener("click", e => {
-//   console.log(add(num1Input.value, num2Input.value, strInput.value, false));
+// numButton.addEventListener("click", (e) =>{
+//    console.log(num1Input.value, num2Input.value, strInput.value, false) 
 // });
-// strButton.addEventListener("click", e => {
-//   console.log(add(num1Input.value, num2Input.value, strInput.value, true));
+// strButton.addEventListener("click", (e) =>{
+//    console.log(num1Input.value, num2Input.value, strInput.value, true)
 // });
-// // write functions that show the numbers, strings, and showResult with the different buttons -  make sure that if a phrase is said the results prints with a phrase, otherwise if not add the two numbers or strings
+// WITH TYPING
+// var numButton = document.querySelector(".add-numbers");
+// var strButton = document.querySelector(".add-with-string");
+// var num1Input = document.querySelector(".num1-input");
+// var num2Input = document.querySelector(".num2-input");
+// var strInput = document.querySelector(".string-input");
+// function add(thing1, thing2, phrase, showString) {
+//     return showString ? phrase + thing1 + thing2 : thing1 + thing2;
+// }
+// numButton.addEventListener("click", function (e) {
+//     var num1 = parseInt(num1Input.value);
+//     var num2 = parseInt(num2Input.value);
+//     console.log(add(num1, num2, strInput.value, false));
+// });
+// strButton.addEventListener("click", function (e) {
+//     var num1 = parseInt(num1Input.value);
+//     var num2 = parseInt(num2Input.value);
+//     console.log(add(num1, num2, strInput.value, true));
+// });
+
 // enum FieldPosition {GOALIE, STRIKER, MIDFIELD, DEFENDER};
 // // pre-declared types
 // const player: {
--- ts-tests.ts
+++ ts-tests.ts
@@ -1,21 +1,21 @@
-//WITHOUT TYPING
-const numButton = document.querySelector(".add-numbers");
-const strButton = document.querySelector(".add-with-string");
-const num1Input = document.querySelector(".num1-input");
-const num2Input = document.querySelector(".num2-input");
-const strInput = document.querySelector(".string-input");
+// //WITHOUT TYPING
+// const numButton = document.querySelector(".add-numbers");
+// const strButton = document.querySelector(".add-with-string");
+// const num1Input = document.querySelector(".num1-input");
+// const num2Input = document.querySelector(".num2-input");
+// const strInput = document.querySelector(".string-input");
 
-function add(thing1, thing2, phrase, showString){
+// function add(thing1, thing2, phrase, showString){
  
-   return showString ? phrase + (thing1 + thing2) : thing1 + thing2
-}
+//    return showString ? phrase + (thing1 + thing2) : thing1 + thing2
+// }
 
-numButton.addEventListener("click", (e) =>{
-   console.log(add(parseInt(num1Input.value), parseInt(num2Input.value), strInput.value, false)) 
-});
-strButton.addEventListener("click", (e) =>{
-   console.log(add(parseInt(num1Input.value), parseInt(num2Input.value), strInput.value, true)) 
-});
+// numButton.addEventListener("click", (e) =>{
+//    console.log(num1Input.value, num2Input.value, strInput.value, false) 
+// });
+// strButton.addEventListener("click", (e) =>{
+//    console.log(num1Input.value, num2Input.value, strInput.value, true)
+// });
 
 // // WITH TYPING
 // const numButton = document.querySelector(".add-numbers")! as HTMLButtonElement;
@@ -30,48 +30,72 @@ strButton.addEventListener("click", (e) =>{
 //   phrase: string,
 //   showString: boolean
 // ) {
-//   return showString ? thing1 + thing2 + phrase : thing1 + thing2;
+//   return showString ? phrase + thing1 + thing2: thing1 + thing2;
 // }
 
 // numButton.addEventListener("click", e => {
-//   console.log(add(num1Input.value, num2Input.value, strInput.value, false));
+//     const num1 = parseInt(num1Input.value)
+//     const num2 = parseInt(num2Input.value)
+//   console.log(add(num1, num2, strInput.value, false));
 // });
+
 // strButton.addEventListener("click", e => {
-//   console.log(add(num1Input.value, num2Input.value, strInput.value, true));
+//     const num1 = parseInt(num1Input.value);
+//     const num2 = parseInt(num2Input.value);
+//   console.log(add(num1, num2, strInput.value, true));
 // });
 
 
-// // write functions that show the numbers, strings, and showResult with the different buttons -  make sure that if a phrase is said the results prints with a phrase, otherwise if not add the two numbers or strings
-// enum FieldPosition {GOALIE, STRIKER, MIDFIELD, DEFENDER};
+
 // // pre-declared types
-// const player: {
+// const person: {
 //     name: 'Bob';
 //     age: 24;
 //     hobbies: string[];
-//     team: [string, number];
-//     position: number;
 // } = {
-//     name : 'Bob',
+//     name: 'Bob',
 //     age: 24,
 //     hobbies: ['books', 'music'],
-//     team: ['AC Milan', 35],
-//     position: FieldPosition.GOALIE
 // }
+
 // //type inference
-// // const player = {
-// //     name : 'Bob',
-// //     age: '24',
-// //     hobbies: ['books', 'music'],
-// //     team: ['AC Milan', 35],
-// //     fieldPosition: FieldPosition.GOALIE
-// // }
+// const player = {
+//     name: 'Bob',
+//     age: '24',
+//     hobbies: ['books', 'music'],
+// }
+
+
+enum FieldPosition {GOALIE, STRIKER, MIDFIELD, DEFENDER};
+// pre-declared types
+const player: {
+    name: 'Bob';
+    age: 24;
+    hobbies: string[];
+    team: [string, number];
+    position: number;
+} = {
+    name : 'Bob',
+    age: 24,
+    hobbies: ['books', 'music'],
+    team: ['AC Milan', 35],
+    position: FieldPosition.GOALIE
+}
+//type inference
+const player = {
+    name : 'Bob',
+    age: '24',
+    hobbies: ['books', 'music'],
+    team: ['AC Milan', 35],
+    fieldPosition: FieldPosition.GOALIE
+}
 
 
 
-// console.log(person.hobbies[1])
-// console.log(person.position)
+console.log(person.hobbies[1])
+console.log(person.position)
 
-// // //make sure to show the errors in array typing
-// // for(const hobby of person.hobbies){
-// //     //can do string manip because we KNOW that the type will be a string 
-// // }
+// //make sure to show the errors in array typing
+// for(const hobby of person.hobbies){
+//     //can do string manip because we KNOW that the type will be a string 
+// }
