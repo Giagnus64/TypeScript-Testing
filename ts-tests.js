d617846b928b3c8bf6b466abb250c9bc1c66c2c9
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2020-01-29 17:18:22 -0500

    Finalized first TS examples

--- ts-tests.js
+++ ts-tests.js
@@ -1,18 +1,65 @@
+//WITHOUT TYPING
 var numButton = document.querySelector(".add-numbers");
-var strButton = document.querySelector(".add-strings");
-var bothButton = document.querySelector(".add-both");
-var numInput = document.querySelector(".num-input");
+var strButton = document.querySelector(".add-with-string");
+var num1Input = document.querySelector(".num1-input");
+var num2Input = document.querySelector(".num2-input");
 var strInput = document.querySelector(".string-input");
-var bothInput = document.querySelector(".both-input");
-function add(thing1, thing2) {
-    return thing1 + thing2;
+function add(thing1, thing2, phrase, showString) {
+    return showString ? phrase + (thing1 + thing2) : thing1 + thing2;
 }
 numButton.addEventListener("click", function (e) {
-    console.log(add(numInput.value, bothInput.value));
+    console.log(add(parseInt(num1Input.value), parseInt(num2Input.value), strInput.value, false));
 });
 strButton.addEventListener("click", function (e) {
-    console.log(add(strInput.value, bothInput.value));
+    console.log(add(parseInt(num1Input.value), parseInt(num2Input.value), strInput.value, true));
 });
-bothButton.addEventListener("click", function (e) {
-    console.log(add(strInput.value, numInput.value));
-});
+// // WITH TYPING
+// const numButton = document.querySelector(".add-numbers")! as HTMLButtonElement;
+// const strButton = document.querySelector(".add-with-string")! as HTMLButtonElement;
+// const num1Input = document.querySelector(".num1-input")! as HTMLInputElement;
+// const num2Input = document.querySelector(".num2-input")! as HTMLInputElement;
+// const strInput = document.querySelector(".string-input")! as HTMLInputElement;
+// function add(
+//   thing1: number,
+//   thing2: number,
+//   phrase: string,
+//   showString: boolean
+// ) {
+//   return showString ? thing1 + thing2 + phrase : thing1 + thing2;
+// }
+// numButton.addEventListener("click", e => {
+//   console.log(add(num1Input.value, num2Input.value, strInput.value, false));
+// });
+// strButton.addEventListener("click", e => {
+//   console.log(add(num1Input.value, num2Input.value, strInput.value, true));
+// });
+// // write functions that show the numbers, strings, and showResult with the different buttons -  make sure that if a phrase is said the results prints with a phrase, otherwise if not add the two numbers or strings
+// enum FieldPosition {GOALIE, STRIKER, MIDFIELD, DEFENDER};
+// // pre-declared types
+// const player: {
+//     name: 'Bob';
+//     age: 24;
+//     hobbies: string[];
+//     team: [string, number];
+//     position: number;
+// } = {
+//     name : 'Bob',
+//     age: 24,
+//     hobbies: ['books', 'music'],
+//     team: ['AC Milan', 35],
+//     position: FieldPosition.GOALIE
+// }
+// //type inference
+// // const player = {
+// //     name : 'Bob',
+// //     age: '24',
+// //     hobbies: ['books', 'music'],
+// //     team: ['AC Milan', 35],
+// //     fieldPosition: FieldPosition.GOALIE
+// // }
+// console.log(person.hobbies[1])
+// console.log(person.position)
+// // //make sure to show the errors in array typing
+// // for(const hobby of person.hobbies){
+// //     //can do string manip because we KNOW that the type will be a string 
+// // }
--- ts-tests.ts
+++ ts-tests.ts
@@ -1,43 +1,77 @@
+//WITHOUT TYPING
 const numButton = document.querySelector(".add-numbers");
-const strButton = document.querySelector(".add-strings");
-const bothButton = document.querySelector(".add-both");
-const numInput = document.querySelector(".num-input")! as HTMLInputElement;
-const strInput = document.querySelector(".string-input")! as HTMLInputElement;
-const bothInput = document.querySelector(".both-input")! as HTMLInputElement;
+const strButton = document.querySelector(".add-with-string");
+const num1Input = document.querySelector(".num1-input");
+const num2Input = document.querySelector(".num2-input");
+const strInput = document.querySelector(".string-input");
 
-function add(thing1: number , thing2: number, phrase: string, showResult: boolean){
-    if(typeof thing1 !== number || typeof thing2 !== number){
-        return "error!"
-    }
-    if(showResult && phrase){
-        console.log
-    }
-   return thing1 + thing2
+function add(thing1, thing2, phrase, showString){
+ 
+   return showString ? phrase + (thing1 + thing2) : thing1 + thing2
 }
 
 numButton.addEventListener("click", (e) =>{
-   console.log(add(numInput.value, bothInput.value)) 
+   console.log(add(parseInt(num1Input.value), parseInt(num2Input.value), strInput.value, false)) 
 });
 strButton.addEventListener("click", (e) =>{
-   console.log(add(strInput.value, bothInput.value)) 
-});
-bothButton.addEventListener("click", (e) =>{
-   console.log(add(strInput.value, numInput.value)) 
+   console.log(add(parseInt(num1Input.value), parseInt(num2Input.value), strInput.value, true)) 
 });
 
-// write functions that show the numbers, strings, and showResult with the different buttons -  make sure that if a phrase is said the results prints with a phrase, otherwise if not add the two numbers or strings
+// // WITH TYPING
+// const numButton = document.querySelector(".add-numbers")! as HTMLButtonElement;
+// const strButton = document.querySelector(".add-with-string")! as HTMLButtonElement;
+// const num1Input = document.querySelector(".num1-input")! as HTMLInputElement;
+// const num2Input = document.querySelector(".num2-input")! as HTMLInputElement;
+// const strInput = document.querySelector(".string-input")! as HTMLInputElement;
 
-const person = {
-    name: 'Bob';
-    age: 24;
-    hobbies: string[];
-} = {
-    name: 'Bob',
-    age: '24',
-    hobbies: ['books', 'music']
-}
+// function add(
+//   thing1: number,
+//   thing2: number,
+//   phrase: string,
+//   showString: boolean
+// ) {
+//   return showString ? thing1 + thing2 + phrase : thing1 + thing2;
+// }
+
+// numButton.addEventListener("click", e => {
+//   console.log(add(num1Input.value, num2Input.value, strInput.value, false));
+// });
+// strButton.addEventListener("click", e => {
+//   console.log(add(num1Input.value, num2Input.value, strInput.value, true));
+// });
 
 
-console.log(person.hobbies[1])
+// // write functions that show the numbers, strings, and showResult with the different buttons -  make sure that if a phrase is said the results prints with a phrase, otherwise if not add the two numbers or strings
+// enum FieldPosition {GOALIE, STRIKER, MIDFIELD, DEFENDER};
+// // pre-declared types
+// const player: {
+//     name: 'Bob';
+//     age: 24;
+//     hobbies: string[];
+//     team: [string, number];
+//     position: number;
+// } = {
+//     name : 'Bob',
+//     age: 24,
+//     hobbies: ['books', 'music'],
+//     team: ['AC Milan', 35],
+//     position: FieldPosition.GOALIE
+// }
+// //type inference
+// // const player = {
+// //     name : 'Bob',
+// //     age: '24',
+// //     hobbies: ['books', 'music'],
+// //     team: ['AC Milan', 35],
+// //     fieldPosition: FieldPosition.GOALIE
+// // }
 
-//make sure to show the errors in array typing
+
+
+// console.log(person.hobbies[1])
+// console.log(person.position)
+
+// // //make sure to show the errors in array typing
+// // for(const hobby of person.hobbies){
+// //     //can do string manip because we KNOW that the type will be a string 
+// // }
