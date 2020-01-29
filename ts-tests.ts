5d322c5af760c9646e31cca88dab3528c394575b
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2020-01-29 16:06:12 -0500

    Added object and array typing

--- ts-tests.ts
+++ ts-tests.ts
@@ -5,7 +5,13 @@ const numInput = document.querySelector(".num-input")! as HTMLInputElement;
 const strInput = document.querySelector(".string-input")! as HTMLInputElement;
 const bothInput = document.querySelector(".both-input")! as HTMLInputElement;
 
-function add(thing1: number , thing2: number){
+function add(thing1: number , thing2: number, phrase: string, showResult: boolean){
+    if(typeof thing1 !== number || typeof thing2 !== number){
+        return "error!"
+    }
+    if(showResult && phrase){
+        console.log
+    }
    return thing1 + thing2
 }
 
@@ -19,3 +25,19 @@ bothButton.addEventListener("click", (e) =>{
    console.log(add(strInput.value, numInput.value)) 
 });
 
+// write functions that show the numbers, strings, and showResult with the different buttons -  make sure that if a phrase is said the results prints with a phrase, otherwise if not add the two numbers or strings
+
+const person = {
+    name: 'Bob';
+    age: 24;
+    hobbies: string[];
+} = {
+    name: 'Bob',
+    age: '24',
+    hobbies: ['books', 'music']
+}
+
+
+console.log(person.hobbies[1])
+
+//make sure to show the errors in array typing
