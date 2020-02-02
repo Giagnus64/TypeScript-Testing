46669a4a881c86dec8a5782d97ca285bec9ea84a
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2020-02-02 16:18:50 -0500

    Fixed a few errors in ts tests

--- ts-tests.js
+++ ts-tests.js
@@ -1,38 +1,34 @@
-// //WITHOUT TYPING
-// const numButton = document.querySelector(".add-numbers");
-// const strButton = document.querySelector(".add-with-string");
-// const num1Input = document.querySelector(".num1-input");
-// const num2Input = document.querySelector(".num2-input");
-// const strInput = document.querySelector(".string-input");
-// function add(thing1, thing2, phrase, showString){
-//    return showString ? phrase + (thing1 + thing2) : thing1 + thing2
+var numButton = document.querySelector(".add-numbers");
+var strButton = document.querySelector(".add-with-string");
+var num1Input = document.querySelector(".num1-input");
+var num2Input = document.querySelector(".num2-input");
+var strInput = document.querySelector(".string-input");
+var answer = document.querySelector(".answer");
+function add(thing1, thing2, phrase, showString) {
+    return showString ? phrase + thing1 + thing2 : thing1 + thing2;
+}
+numButton.addEventListener("click", function (e) {
+    answer.innerText = add(num1Input.value, num2Input.value, strInput.value, false);
+});
+strButton.addEventListener("click", function (e) {
+    answer.innerText = add(num1Input.value, num2Input.value, strInput.value, true);
+});
+// // pre-declared types
+// const person: {
+//     name: 'Bob';
+//     age: 24;
+//     hobbies: string[];
+// } = {
+//     name: 'Bob',
+//     age: 24,
+//     hobbies: ['books', 'music'],
 // }
-// numButton.addEventListener("click", (e) =>{
-//    console.log(num1Input.value, num2Input.value, strInput.value, false) 
-// });
-// strButton.addEventListener("click", (e) =>{
-//    console.log(num1Input.value, num2Input.value, strInput.value, true)
-// });
-// WITH TYPING
-// var numButton = document.querySelector(".add-numbers");
-// var strButton = document.querySelector(".add-with-string");
-// var num1Input = document.querySelector(".num1-input");
-// var num2Input = document.querySelector(".num2-input");
-// var strInput = document.querySelector(".string-input");
-// function add(thing1, thing2, phrase, showString) {
-//     return showString ? phrase + thing1 + thing2 : thing1 + thing2;
+// //type inference
+// const player = {
+//     name: 'Bob',
+//     age: '24',
+//     hobbies: ['books', 'music'],
 // }
-// numButton.addEventListener("click", function (e) {
-//     var num1 = parseInt(num1Input.value);
-//     var num2 = parseInt(num2Input.value);
-//     console.log(add(num1, num2, strInput.value, false));
-// });
-// strButton.addEventListener("click", function (e) {
-//     var num1 = parseInt(num1Input.value);
-//     var num2 = parseInt(num2Input.value);
-//     console.log(add(num1, num2, strInput.value, true));
-// });
-
 // enum FieldPosition {GOALIE, STRIKER, MIDFIELD, DEFENDER};
 // // pre-declared types
 // const player: {
@@ -49,16 +45,69 @@
 //     position: FieldPosition.GOALIE
 // }
 // //type inference
-// // const player = {
-// //     name : 'Bob',
-// //     age: '24',
-// //     hobbies: ['books', 'music'],
-// //     team: ['AC Milan', 35],
-// //     fieldPosition: FieldPosition.GOALIE
-// // }
+// const player = {
+//     name : 'Bob',
+//     age: '24',
+//     hobbies: ['books', 'music'],
+//     team: ['AC Milan', 35],
+//     fieldPosition: FieldPosition.GOALIE
+// }
 // console.log(person.hobbies[1])
 // console.log(person.position)
-// // //make sure to show the errors in array typing
-// // for(const hobby of person.hobbies){
-// //     //can do string manip because we KNOW that the type will be a string 
+// //make sure to show the errors in array typing
+// for(const hobby of person.hobbies){
+//     //can do string manip because we KNOW that the type will be a string 
+// }
+//union types
+// function add(thing1: number | string, thing2: number | string){
+//     let result
+//     if(typeof thing1 === 'number' && typeof thing2 === 'number'){
+//         result = thing1 + thing2
+//     } else {
+//         result = input1.toString() + input2.toString()
+//     }
+//     return result
+// }
+// type FieldPosition = 'GOALIE' | 'STRIKER'|'MIDFIELD'|'DEFENDER' 
+// interface PlayerConfig {
+//     name: string;
+//     age: number;
+//     hobbies?: string[];
+//     position: FieldPosition
+// }
+// //Literal and Type Aliases
+// const player: PlayerConfig = {
+//     name: "Bob",
+//     age: 23,
+//     hobbies: ["books", "music"],
+//     position: 'GOALIE'
+// }
+// class Player implements PlayerConfig {
+//     constructor(public name: string, public age: number, public position: FieldPosition){}
+// }
+// const dude = new Player("Dan", 23, 'GOALIE')
+// check the cool Class/Interface stuff
+// Functions Return Types
+// function add(thing1: number, thing2: number): string{
+//     return thing1 + thing2
+// }
+// function add(thing1: number, thing2: number): number{
+//     return thing1 + thing2
+// }
+// // function printGreeting(name: string){
+// //     console.log(`Hello, ${name}!`)
 // // }
+// function printGreeting(name: string): void{
+//     console.log(`Hello, ${name}!`)
+// }
+// //Function types
+// //let greetUser = Function;
+// let greetUser: (a: string) => void;
+// greetUser = add;
+// greetUser = printGreeting;
+//console.log(greetUser('bob'))
+//unknown
+// let userInput: unknown;
+// function generateError(message: string, code: number): never {
+//     throw {message, code}
+// }
--- ts-tests.ts
+++ ts-tests.ts
@@ -1,28 +1,9 @@
-// //WITHOUT TYPING
-// const numButton = document.querySelector(".add-numbers");
-// const strButton = document.querySelector(".add-with-string");
-// const num1Input = document.querySelector(".num1-input");
-// const num2Input = document.querySelector(".num2-input");
-// const strInput = document.querySelector(".string-input");
-
-// function add(thing1, thing2, phrase, showString){
- 
-//    return showString ? phrase + (thing1 + thing2) : thing1 + thing2
-// }
-
-// numButton.addEventListener("click", (e) =>{
-//    console.log(num1Input.value, num2Input.value, strInput.value, false) 
-// });
-// strButton.addEventListener("click", (e) =>{
-//    console.log(num1Input.value, num2Input.value, strInput.value, true)
-// });
-
-// // WITH TYPING
 // const numButton = document.querySelector(".add-numbers")! as HTMLButtonElement;
 // const strButton = document.querySelector(".add-with-string")! as HTMLButtonElement;
 // const num1Input = document.querySelector(".num1-input")! as HTMLInputElement;
 // const num2Input = document.querySelector(".num2-input")! as HTMLInputElement;
 // const strInput = document.querySelector(".string-input")! as HTMLInputElement;
+// const answer = document.querySelector(".answer")! as any;
 
 // function add(
 //   thing1: number,
@@ -34,29 +15,34 @@
 // }
 
 // numButton.addEventListener("click", e => {
-//     const num1 = parseInt(num1Input.value)
-//     const num2 = parseInt(num2Input.value)
-//   console.log(add(num1, num2, strInput.value, false));
+//   answer.innerText = add(
+//     num1Input.value,
+//     num2Input.value,
+//     strInput.value,
+//     false
+//   );
 // });
-
 // strButton.addEventListener("click", e => {
-//     const num1 = parseInt(num1Input.value);
-//     const num2 = parseInt(num2Input.value);
-//   console.log(add(num1, num2, strInput.value, true));
+//   answer.innerText = add(
+//     num1Input.value,
+//     num2Input.value,
+//     strInput.value,
+//     true
+//   );
 // });
 
 
 
-// // pre-declared types
-// const person: {
-//     name: 'Bob';
-//     age: 24;
-//     hobbies: string[];
-// } = {
-//     name: 'Bob',
-//     age: 24,
-//     hobbies: ['books', 'music'],
-// }
+// pre-declared types
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+} = {
+    name: 'Bob',
+    age: 24,
+    hobbies: ['books', 'music'],
+}
 
 // //type inference
 // const player = {
@@ -92,8 +78,8 @@
 
 
 
-console.log(person.hobbies[1])
-console.log(person.position)
+// console.log(person.hobbies[1])
+// console.log(person.position)
 
 // //make sure to show the errors in array typing
 // for(const hobby of person.hobbies){
@@ -113,27 +99,27 @@ console.log(person.position)
 //     return result
 // }
 
-type FieldPosition = 'GOALIE' | 'STRIKER'|'MIDFIELD'|'DEFENDER' 
+// type FieldPosition = 'GOALIE' | 'STRIKER'|'MIDFIELD'|'DEFENDER' 
 
-interface PlayerConfig {
-    name: string;
-    age: number;
-    hobbies?: string[];
-    position: FieldPosition
-}
+// interface PlayerConfig {
+//     name: string;
+//     age: number;
+//     hobbies?: string[];
+//     position: FieldPosition
+// }
 
-//Literal and Type Aliases
-const player: PlayerConfig = {
-    name: "Bob",
-    age: 23,
-    hobbies: ["books", "music"],
-    position: 'GOALIE'
-}
-class Player implements PlayerConfig {
-    constructor(public name: string, public age: number, public position: FieldPosition){}
-}
+// //Literal and Type Aliases
+// const player: PlayerConfig = {
+//     name: "Bob",
+//     age: 23,
+//     hobbies: ["books", "music"],
+//     position: 'GOALIE'
+// }
+// class Player implements PlayerConfig {
+//     constructor(public name: string, public age: number, public position: FieldPosition){}
+// }
 
-const dude = new Player("Dan", 23, 'GOALIE')
+// const dude = new Player("Dan", 23, 'GOALIE')
 
 // check the cool Class/Interface stuff
 
@@ -144,22 +130,22 @@ const dude = new Player("Dan", 23, 'GOALIE')
 //     return thing1 + thing2
 // }
 
-function add(thing1: number, thing2: number): number{
-    return thing1 + thing2
-}
+// function add(thing1: number, thing2: number): number{
+//     return thing1 + thing2
+// }
 
-// function printGreeting(name: string){
+// // function printGreeting(name: string){
+// //     console.log(`Hello, ${name}!`)
+// // }
+// function printGreeting(name: string): void{
 //     console.log(`Hello, ${name}!`)
 // }
-function printGreeting(name: string): void{
-    console.log(`Hello, ${name}!`)
-}
 
-//Function types
-//let greetUser = Function;
-let greetUser: (a: string) => void;
-greetUser = add;
-greetUser = printGreeting;
+// //Function types
+// //let greetUser = Function;
+// let greetUser: (a: string) => void;
+// greetUser = add;
+// greetUser = printGreeting;
 //console.log(greetUser('bob'))
 
 //unknown
